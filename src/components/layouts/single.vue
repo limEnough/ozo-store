a17656ce92ced6aba31a578d88111fc6f98b1057
limEnough <limEnough@gmail.com>
2024-02-02 22:39:48 +0900

    feat: 싱글 페이지 레이아웃 추가

---
+++ src/components/layouts/single.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="single-layout">
+    <div class="single-layout__inner">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import '@/styles/components/layouts/single.scss';
+</style>
