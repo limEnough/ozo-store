64eeb4c1519bed8917228117aad9f5a86669fa9a
limEnough <limEnough@gmail.com>
2024-02-15 20:41:42 +0900

    feat: set default layout

---
+++ src/components/layouts/default-layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="default-layout">
+    <HeaderLayout></HeaderLayout>
+
+    <div class="default-layout__inner">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import HeaderLayout from '@/components/layouts/header-layout.vue';
+</script>
+
+<style lang="scss" scoped>
+  @import '@/styles/components/layouts/default-layout.scss';
+</style>
