996b6aa0d12eacf47b5c720192e6fa3cbe75bed4
limEnough <limEnough@gmail.com>
2024-02-27 14:36:05 +0900

    feat: input-phone component 추가

---
+++ src/components/elements/input-phone.vue
@@ -0,0 +1,72 @@
+<template>
+  <div
+    class="input-phone-component"
+    v-bind="styleAttrs"
+  >
+    <InputNumber
+      v-model="frontNumber"
+      v-bind="functionalAttrs"
+      :min-length="2"
+      :max-length="4"
+      :use-length-count="false"
+      :use-comma="false"
+      :disabled="disabled"
+      name="frontNumber"
+      class="input-phone-component__input"
+      type="text"
+      use-zero
+    ></InputNumber>
+
+    <span class="input-phone-component__split">-</span>
+
+    <InputNumber
+      v-model="middleNumber"
+      v-bind="functionalAttrs"
+      :min-length="3"
+      :max-length="4"
+      :use-length-count="false"
+      :use-comma="false"
+      :disabled="disabled"
+      name="middleNumber"
+      class="input-phone-component__input"
+      type="text"
+      use-zero
+    ></InputNumber>
+
+    <span class="input-phone-component__split">-</span>
+
+    <InputNumber
+      v-model="endNumber"
+      v-bind="functionalAttrs"
+      :max-length="4"
+      :use-length-count="false"
+      :use-comma="false"
+      :disabled="disabled"
+      name="endNumber"
+      class="input-phone-component__input"
+      type="text"
+      use-zero
+    ></InputNumber>
+  </div>
+</template>
+
+<script lang="ts">
+  // declare additional options
+  export default {
+    inheritAttrs: false,
+  };
+</script>
+
+<script setup lang="ts">
+  import InputPhoneComposable, { InputPhoneEmits, InputPhoneProps } from '@/composables/elements/input-phone';
+  import InputNumber from '@/components/elements/input-number.vue';
+
+  const emits = defineEmits(InputPhoneEmits);
+  const props = defineProps(InputPhoneProps);
+
+  const { frontNumber, middleNumber, endNumber, styleAttrs, functionalAttrs } = InputPhoneComposable(emits, props);
+</script>
+
+<style lang="scss" scoped>
+  @import '@/styles/components/elements/input-phone.scss';
+</style>
---
+++ src/composables/elements/input-phone.ts
@@ -0,0 +1,96 @@
+import { type PropType, computed, onMounted, ref, useAttrs, watch } from 'vue';
+import type { CustomEmit } from '@/types/common.types';
+
+type Emits = 'update:phoneNumber';
+const emits: Emits[] = ['update:phoneNumber'];
+
+interface Props {
+  phoneNumber: string;
+  disabled: boolean;
+}
+
+const props = {
+  phoneNumber: {
+    type: String as PropType<Props['phoneNumber']>,
+    required: true as const,
+  },
+  disabled: {
+    type: Boolean as PropType<Props['disabled']>,
+    default: false,
+  },
+};
+
+export default function InputPhoneComposable(emit: CustomEmit<Emits>, props: Props) {
+  const attrs = useAttrs();
+
+  const frontNumber = ref('');
+  const middleNumber = ref('');
+  const endNumber = ref('');
+
+  const fullNumber = computed(() => {
+    const first = frontNumber.value;
+    const middle = '-' + middleNumber.value;
+    const last = endNumber.value ? '-' + endNumber.value : '';
+    // const last = endNumber.value.includes('**') || regex.end.test(endNumber.value) ? '-' + endNumber.value : '';
+    return first + middle + last;
+  });
+
+  const styleAttrs = computed(() => {
+    if (attrs.class) return { class: attrs.class };
+    else if (attrs.style) return { class: attrs.style };
+    else return {};
+  });
+
+  const functionalAttrs = computed(() => {
+    if (attrs.class) return { ...attrs, class: '' };
+    else return attrs;
+  });
+
+  /**
+   * @description 각 자리수 할당 함수
+   *
+   * @param phoneNumber
+   */
+  const setPhoneNumber = (phoneNumber: string) => {
+    let [front, middle, end] = phoneNumber && phoneNumber.includes('-') ? phoneNumber.split('-') : [phoneNumber || ''];
+
+    middle = middle?.trim() ?? '';
+    end = end?.trim() ?? '';
+
+    frontNumber.value = front;
+    middleNumber.value = middle;
+    endNumber.value = end;
+  };
+
+  onMounted(() => {
+    watch(
+      () => props.phoneNumber,
+      (newVal) => {
+        if (newVal === fullNumber.value) return;
+        setPhoneNumber(props.phoneNumber);
+      },
+    );
+
+    watch(fullNumber, (newFullNumber) => {
+      emit('update:phoneNumber', newFullNumber);
+    });
+  });
+
+  const init = () => {
+    setPhoneNumber(props.phoneNumber);
+    emit('update:phoneNumber', fullNumber.value);
+  };
+
+  init();
+
+  return {
+    frontNumber,
+    middleNumber,
+    endNumber,
+
+    styleAttrs,
+    functionalAttrs,
+  };
+}
+
+export { emits as InputPhoneEmits, props as InputPhoneProps };
