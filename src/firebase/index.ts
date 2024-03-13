6164df941400795bde93a9971535405e897d7156
limEnough <limEnough@gmail.com>
2024-03-13 14:45:42 +0900

    fix: ssr 환경 시 window 못 찾는 error로 인해 getAnalytics 사용 삭제

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from 'firebase/app';
-import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
@@ -14,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-getAnalytics(app);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
