cce2d8540f6fc89d44e573230af0813ae3185052
limEnough <limEnough@gmail.com>
2024-03-13 22:28:06 +0900

    fix: 최상위 레벨 코드에서 await 하여 발생한 error 수정

--- src/main.ts
+++ src/main.ts
@@ -7,19 +7,23 @@ import vueCookies from 'vue-cookies';
 import App from './App.vue';
 import type { VueContext } from './types/common.types';
 
-const app = createApp(App);
-const pinia = createPinia();
-const router = await createRouter();
-const vueContext: VueContext = { store: pinia, router };
-const i18n = await createCustomI18n({
-  lang: 'ko',
-});
+const createProject = async () => {
+  const app = createApp(App);
+  const pinia = createPinia();
+  const router = await createRouter();
+  const vueContext: VueContext = { store: pinia, router };
+  const i18n = await createCustomI18n({
+    lang: 'ko',
+  });
 
-addOnRouter(vueContext);
+  addOnRouter(vueContext);
 
-app.use(pinia);
-app.use(router);
-app.use(i18n);
-app.use(vueCookies);
+  app.use(pinia);
+  app.use(router);
+  app.use(i18n);
+  app.use(vueCookies);
 
-app.mount('#app');
+  app.mount('#app');
+};
+
+createProject();
