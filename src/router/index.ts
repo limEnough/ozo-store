12af94075e75e31fdc9e6b85b74a35c644e50f0c
limEnough <limEnough@gmail.com>
2024-03-14 17:49:48 +0900

    feat: 유저 접근 가능 페이지 미로그인 상태로 접근 시, 로그인페이지로 리다이렉션 처리

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter as vueCreateRouter, type RouteRecordName
 import appConfig from '@/configs/app.config';
 import { useLayoutStore } from '@/stores/layout';
 import type { VueContext } from '@/types/common.types';
-import { MAIN_PAGE_NAMES } from '@/constants/path-constants';
+import { MAIN_PAGE_NAMES, MEMBER_PAGE_NAMES } from '@/constants/path-constants';
 import { getRoutes } from './routes';
 
 export async function createRouter() {
@@ -20,14 +20,24 @@ export async function createRouter() {
 // 라우트 이동 전 옵션 설정
 export const addOnRouter = (vueContext: VueContext) => {
   const mainPath = vueContext.router.resolve({ name: MAIN_PAGE_NAMES['main'] }).path;
+  const loginPath = vueContext.router.resolve({ name: MEMBER_PAGE_NAMES['member-login'] }).path;
 
   const { NonUserAccessibleNames, UserAccessibleNames, UserRedirectNames } = appConfig.routerAccess;
 
   vueContext.router.beforeEach((to: any) => {
     const layoutStore = useLayoutStore();
 
-    if (UserRedirectNames.includes(to.name as RouteRecordName) && layoutStore.isLoggedIn) {
-      return mainPath;
+    // 로그인 케이스
+    if (layoutStore.isLoggedIn) {
+      if (UserRedirectNames.includes(to.name as RouteRecordName)) {
+        return mainPath;
+      }
+    }
+    // 비로그인 case
+    else {
+      if (UserAccessibleNames.includes(to.name as RouteRecordName)) {
+        return loginPath;
+      }
     }
   });
 };
