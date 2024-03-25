20346900ddf6de2919ebc659457d2b616e2b2c48
limEnough <limEnough@gmail.com>
2024-03-25 14:29:22 +0900

    feat: empty component 추가

---
+++ src/components/elements/empty.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="empty-component">
+    <!-- empty 문구 영역 -->
+    <div class="empty-component__text">
+      <slot name="text"> No Results! </slot>
+    </div>
+
+    <!-- empty 버튼 영역 -->
+    <div class="empty-component__action">
+      <slot name="button"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+  // declare additional options
+  export default {
+    inheritAttrs: false,
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '@/styles/components/elements/empty.scss';
+</style>
