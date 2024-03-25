f652bafeefc4bf990ee46ec4af5de3c5af4e5f1a
limEnough <limEnough@gmail.com>
2024-03-25 15:30:11 +0900

    feat: 페이지 중비중 컴포넌트 추가

---
+++ src/components/elements/ready.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="ready-component">
+    <p class="ready-component__text">Preparing the page...</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  @import '@/styles/components/elements/ready.scss';
+</style>
