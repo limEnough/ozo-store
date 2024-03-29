16312afdb68f4807ac069a76038d604c4ca58e39
limEnough <limEnough@gmail.com>
2024-03-29 17:03:29 +0900

    feat: 상품 컴포넌트 파일 생성

---
+++ src/components/modules/goods.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="goods-component"></div>
+</template>
+
+<script lang="ts">
+  // declare additional options
+  export default {
+    inheritAttrs: false,
+  };
+</script>
+
+<script setup lang="ts">
+  // import goodsComposable, {goodsEmits, goodsProps} from '@/composables/modules/goods'
+
+  // const emits = defineEmits(goodsEmits);
+  // const props = defineProps(goodsProps);
+
+  // const {  } = goodsComposable(emits, props);
+</script>
+
+<style lang="scss" scoped>
+  /* @import '@/styles/components/modules/goods.scss'; */
+</style>
